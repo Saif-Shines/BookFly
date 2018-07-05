6786e3dea8ddec1dab4d7b1c876b075f141d9194
Shaik Saif Ali <saifshine7@gmail.com>
2018-07-05 14:15:12 +0530

    Final changes #1

--- app/src/main/java/driver/SampleTests.java
+++ app/src/main/java/driver/SampleTests.java
@@ -21,8 +21,9 @@ public class SampleTests {
   public void testGetClient() throws Exception {
     
     Driver.uploadClientInfo(CLIENTS);
-    
-    String expected = "Roe,Richard,richard@email.com,21 First Lane Way,9999888877776666,2017-10-01";
+    /**
+     * This code learnt form res: */
+    String expected = "Saif,Anuhya,saif@email.com,21 First Lane Way,99993514776666,2017-10-01";
     String found = Driver.getClient("richard@email.com").trim();
     String msg = "Unexpected (incorrect or incorrectly formatted) "
         + "client information or string output was returned.";
@@ -36,7 +37,7 @@ public class SampleTests {
 
     Driver.uploadFlightInfo(FLIGHTS1);
     
-    String expected = "490,2016-09-30 22:40,2016-10-01 01:59,Go Airline,New York,Boston,532.00";
+    String expected = "567,2006-09-30 22:40,2016-10-01 01:59,Jet Airways,India,New Delhi,532.00";
     String found = Driver.getFlights("2016-09-30", "New York", "Boston").trim();
     String msg = "Unexpected (incorrect or incorrectly formatted) "
         + "flight information or string output was returned.";
@@ -53,13 +54,13 @@ public class SampleTests {
     Driver.uploadFlightInfo(FLIGHTS2);
     
     String expected = 
-        "490,2016-09-30 22:40,2016-10-01 01:59,Go Airline,London,Rome\n532.99\n03:19\n"
-        + "102,2016-09-30 16:37,2016-09-30 17:22,Go Airline,London,Paris\n"
-        + "249,2016-09-30 19:22,2016-09-30 22:40,Go Airline,Paris,Rome\n580.00\n06:03";
+        "490,2016-09-30 22:40,2016-10-01 01:59,Air India,Jupiter,blackhole\n532.99\n03:19\n"
+        + "102,2016-09-30 16:37,2016-09-30 17:22,Air India,Moon,waterplace\n"
+        + "249,2016-09-30 19:22,2016-09-30 22:40,Some Airlines,Headwatch,Rome\n580.00\n06:03";
     String expected2 = 
-        "102,2016-09-30 16:37,2016-09-30 17:22,Go Airline,London,Paris\n"
-        + "249,2016-09-30 19:22,2016-09-30 22:40,Go Airline,Paris,Rome\n580.00\n06:03\n"
-        + "490,2016-09-30 22:40,2016-10-01 01:59,Go Airline,London,Rome\n532.99\n03:19";
+        "102,2016-09-30 16:37,2016-09-30 17:22,Air India,London,Paris\n"
+        + "249,2016-09-30 19:22,2016-09-30 22:40,Air India,Paris,Rome\n580.00\n06:03\n"
+        + "490,2016-09-30 22:40,2016-10-01 01:59,Air India,London,Rome\n532.99\n03:19";
     String found = Driver.getItineraries("2016-09-30", "London", "Rome").trim();
     
     String msg = "Unexpected (incorrect or incorrectly formatted) "
